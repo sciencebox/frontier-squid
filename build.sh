f9aa7a64e482ea424c32abca3603226e3349f16d
Enrico Bocchi <enrico.bocchi@cern.ch>
2022-02-02 19:35:33 +0100

    build: Add automated build and push script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Tag name
+BASE_TAG='gitlab-registry.cern.ch/sciencebox/docker-images/frontier-squid'
+
+# Specify the frontier-squid version to install (or comment out to use the latest)
+SQUID_VERSION='4.15-2.1'
+
+# Build the Docker image
+if [ -z $SQUID_VERSION ]; then
+  TAG="$BASE_TAG:latest"
+  docker build -t $TAG .
+else
+  TAG="$BASE_TAG:$SQUID_VERSION"
+  docker build --build-arg SQUID_VERSION=-$SQUID_VERSION -t $TAG .
+fi
+
+# Push the image to the GitLab registry
+docker login gitlab-registry.cern.ch
+docker push $TAG
