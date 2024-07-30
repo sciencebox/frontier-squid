5d0f210d4c825c607bd579840d43349b9e1d32af
Ryan Taylor <1686627+rptaylor@users.noreply.github.com>
2024-07-30 15:25:24 -0700

    back to SQUID_VERSION because of Dockerfile arg

--- build.sh
+++ build.sh
@@ -5,18 +5,17 @@ BASE_TAG='gitlab-registry.cern.ch/sciencebox/docker-images/frontier-squid'
 REGISTRY_URL='gitlab-registry.cern.ch'
 
 # Specify the frontier-squid and Enterprise Linux version
-SQUID_VERSION='4.17-2.1'
 EL_VERSION='el9'
 # Optionally comment this out if you want to use 'latest' by default
-TAG_VERSION="${SQUID_VERSION}.${EL_VERSION}"
+SQUID_VERSION="4.17-2.1.${EL_VERSION}"
 
 # Build the Docker image
-if [ -z $TAG_VERSION ]; then
+if [ -z $SQUID_VERSION ]; then
   TAG="$BASE_TAG:latest"
   docker build -t $TAG .
 else
-  TAG="$BASE_TAG:$TAG_VERSION"
-  docker build --build-arg TAG_VERSION=-$TAG_VERSION -t $TAG .
+  TAG="$BASE_TAG:$SQUID_VERSION"
+  docker build --build-arg SQUID_VERSION=-$SQUID_VERSION -t $TAG .
 fi
 
 # Push the image to the GitLab registry
